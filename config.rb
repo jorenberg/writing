6133793d2326d939438d55d245542db9845d2ff8
Sequømics <sequomics@gmail.com>
2015-12-05 20:54:06 +0530

    Initial commit
    Initials of Build System of Writing®.
    Help: http://compass-style.org/.

--- config.rb
+++ config.rb
@@ -1,1 +1,18 @@
 #!/usr/bin/env ruby
+
+
+# AXIX®-Pro
+# From the Desk of Sequømics Corporation.
+# @author     : Prabhat Kumar
+# @copyright  : Sequømics Corporation
+# @homepage   : http://sequomics.com/
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Everest
+# @date       : 01-Dec-2015
+# @license    : Apache, version 2.0
+# ---------------------------------------
+# Principle  : Standard Module Definition
+# ---------------------------------------
