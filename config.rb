1471c865f7c99af9ef0968cabb2bb89ab0cb7eb8
Sequømics <sequomics@gmail.com>
2015-12-06 00:21:13 +0530

    Initial commit
    config.rb, is used to compile compass based framework, --/The Heart of Beauty/-- of "Writing®".
    Help: http://compass-style.org/.
    Usage: $ compass compile

--- config.rb
+++ config.rb
@@ -43,3 +43,7 @@ line_comments = true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+# Usage: $ compass version
+# Usage: $ compass compile
