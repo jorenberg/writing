558b821f8afb976905b17e5d34d2cd5521e7bd6f
Sequømics <sequomics@gmail.com>
2015-12-05 20:24:05 +0530

    Initial commit
    publish.sh, to --/publish/-- after completion of "Grunt-Build, Grunt-Test & Grunt-TravisCI", for "Writing®".
    Usage: $ sh publish.sh

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# ====================================================================
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2015, Sequømics Corporation. All rights reserved.
+# ====================================================================
