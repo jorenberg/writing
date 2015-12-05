238d71774bca7bb444e3306d52117c0629abf148
Sequømics <sequomics@gmail.com>
2015-12-06 00:56:46 +0530

    Initial commit
    Initials of Test System of Writing®.
    Help: https://github.com/karma-runner/karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Writing® [v1.0.0]
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 01-Dec-2015
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "Writing®".
+ * ______________________________________________________________
+ */
