fd723db16b141cec9159fe121f24060b569f24c6
Sequømics <sequomics@gmail.com>
2015-12-06 21:56:05 +0530

    Initial commit
    "import os" module provides a portable way of using operating system dependent functionality.

--- source/core/fonts/compare.py
+++ source/core/fonts/compare.py
@@ -28,3 +28,5 @@ __license__     = 'Apache License'
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
+
+import os
